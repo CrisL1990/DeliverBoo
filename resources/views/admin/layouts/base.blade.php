4260718b3e8d14aef40a7599a517ed1d2a6a3f97
yuribertoli <yuribertoli@yahoo.it>
2022-05-05 13:19:56 +0200

    lato client

--- resources/views/admin/layouts/base.blade.php
+++ resources/views/admin/layouts/base.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -94,7 +95,11 @@
             </div>
 
             @yield('content')
+
+            
         </main>
     </div>
+
+
 </body>
 </html>
