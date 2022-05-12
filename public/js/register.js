42f94c393f0612919c69b0e7889bcd813e5a52ce
yuribertoli <yuribertoli@yahoo.it>
2022-05-12 12:38:40 +0200

    cl

--- public/js/register.js
+++ public/js/register.js
@@ -17495,7 +17495,7 @@ function validaEmail(str) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\booleanLaravel\DeliverBoo\resources\js\register.js */"./resources/js/register.js");
+module.exports = __webpack_require__(/*! C:\Users\Henry\Documents\Boolean\GitHub\DeliverBoo\resources\js\register.js */"./resources/js/register.js");
 
 
 /***/ })
