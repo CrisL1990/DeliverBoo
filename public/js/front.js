58a43364dcd39be40b1e0cb34d802dd21303aeaf
Matteo Belcari <matteobelcari977@gmail.com>
2022-05-19 16:48:06 +0200

    commit

--- public/js/front.js
+++ public/js/front.js
@@ -73500,7 +73500,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Henry\Documents\Boolean\GitHub\DeliverBoo\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\Roberto\Desktop\boolean rogrammi\DeliverBoo\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
